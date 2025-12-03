3e54633b8e90d714dbdb75a695e9665189c8c017
IalwaysLai <llai@cpp.edu>
2025-12-03 15:45:33 -0800

    Creation of CSVReader h and cpp files
    Might add comments for explanations of each line

---
+++ Code/CSVReader.cpp
@@ -0,0 +1,54 @@
+#include "CSVReader.h"
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+CSVReader::CSVReader(const string& filePath) : filePath(filePath) {}
+
+string CSVReader::getFilePath() const {
+    return filePath;
+}
+
+void CSVReader::setFilePath(const string& newFilePath) {
+    filePath = newFilePath;
+}
+
+vector<SensorData> CSVReader::readData() const {
+    vector<SensorData> dataList;
+    ifstream file(filePath);
+//for debugging, might remove later?
+    if (!file.is_open()) {
+        cerr << "Error: Could not open file " << filePath << endl;
+        return dataList;
+    }
+
+    string line;
+    // Skip header
+    getline(file, line);
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string ts, device, co, humidity, light, lpg, motion, smoke, temp;
+
+        getline(ss, ts, ',');
+        getline(ss, device, ',');
+        getline(ss, co, ',');
+        getline(ss, humidity, ',');
+        getline(ss, light, ',');
+        getline(ss, lpg, ',');
+        getline(ss, motion, ',');
+        getline(ss, smoke, ',');
+        getline(ss, temp, ',');
+
+        SensorData row(
+            stol(ts), device, stod(co), stod(humidity), (light == "TRUE"), stod(lpg), (motion == "TRUE"), stod(smoke), stod(temp)
+        );
+
+        dataList.push_back(row);
+    }
+
+    file.close();
+    return dataList;
+}
