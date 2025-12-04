18c5835017837098da7eec59f0c5db55fa03c2a0
mariec-j <mcjardiniano@cpp.edu>
2025-12-03 20:17:49 -0800

    Readding CSVReader.h
    - content was never uploaded somehow, so the .h is created to remove errors in the .cpp

---
+++ Code/CSVReader.h
@@ -0,0 +1,25 @@
+#ifndef CSVREADER_H
+#define CSVREADER_H
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include "SensorData.h"
+using namespace std;
+
+class CSVReader {
+    public:
+        CSVReader(const string& filePath);
+
+        string getFilePath() const;
+
+        void setFilePath(const string& newFilePath);
+
+        vector<SensorData> readData() const;
+
+    private:
+        string filePath;
+};
+
+#endif
