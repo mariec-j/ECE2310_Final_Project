eaa4018a83dcad5ed2518670e7f0208594e04dfb
Naomi-ANN <nevellalex@gmail.com>
2025-12-03 19:44:46 -0800

    Update DataProcessor.cpp

--- Code/DataProcessor.cpp
+++ Code/DataProcessor.cpp
@@ -22,22 +22,50 @@ void DataProcessor::SetProcessedData(list<SensorData> data){
    // list<SensorData> processed;
 //}
 
+
+
+
+
+
 map<string,list<SensorData>> DataProcessor::groupByDevice(list<SensorData> data){
     map<string, list<SensorData>> groupedData;
 
-//does this work, nooo???
-string deviceID = SensorData.getDeviceID();
+//key is strign  and we are gonna put deviceID into the string
+//deviceID is key, and each string var has an int that is assigned to it
+//"string"--> 0
+//"string"--> 1
+//....
 
+//this is how you go through a list and we need this bc we need to go thru the entire list of DeviceID
+//auto
+for(const auto& dataElement: data){
+
+//getting/setting deivceID by accessing it from SensorData and retriving the value thrui getDeviceID
+string deviceID = SensorData.getDeviceID();
+//getDeviceID will either add the DeviceID to the key or if already added will just access DeviceID
+
+//we are now indexing our dataID assigning a hmerical value to each row under device
+groupedData[deviceID].push_back(SensorData);
 }
+return groupedData;
+}
+
+
+
+
+
+
+
+
 
 //should it be temp or TempC does it matter?
 void ProcessedData::convertTempToCelcius(){
    
     double TempF = SensorData.getTemp();
    
-    double TempC = (5.0/9.0) * (TempF - 32.0);
+    double temp = (5.0/9.0) * (TempF - 32.0);
 
-    SensorData.setTemp(TempC);
+    SensorData.setTemp(temp);
 }
 
 
