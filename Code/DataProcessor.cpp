ecb8db23d8a9b363b43cb688786ab7d4b212e317
mariec-j <mcjardiniano@cpp.edu>
2025-12-03 20:07:10 -0800

    Debugging Data Processor

--- Code/DataProcessor.cpp
+++ Code/DataProcessor.cpp
@@ -16,13 +16,8 @@ void DataProcessor::SetProcessedData(list<SensorData> data){
     ProcessedData = data; //data is the var we passed thru in .h
 }
 
-//idk, its tricky now
-//process data is convert temp to celcius
-//list<SensorData> DataProcessor::processData(list<SensorData> data){
-   // list<SensorData> processed;
-//}
 
-map<string,list<SensorData>> DataProcessor::groupByDevice(list<SensorData> data){
+map<string,list<SensorData>> DataProcessor::groupByDevice(list<SensorData> data) {
     map<string, list<SensorData>> groupedData;
 
 //key is strign  and we are gonna put deviceID into the string
@@ -33,26 +28,31 @@ map<string,list<SensorData>> DataProcessor::groupByDevice(list<SensorData> data)
 
 //this is how you go through a list and we need this bc we need to go thru the entire list of DeviceID
 //auto
-for(const auto& dataElement: data){
+    for(const auto& dataElement: data) {
 
 //getting/setting deivceID by accessing it from SensorData and retriving the value thrui getDeviceID
-string deviceID = dataElement.getDeviceID();
-//getDeviceID will either add the DeviceID to the key or if already added will just access DeviceID
+    string deviceID = dataElement.getDeviceID();
 
+//getDeviceID will either add the DeviceID to the key or if already added will just access DeviceID
 //we are now indexing our dataID assigning a hmerical value to each row under device
-groupedData[deviceID].push_back(dataElement);
-}
-return groupedData;
+    groupedData[deviceID].push_back(dataElement);
+    }
+    
+    return groupedData;
 }
 
 //should it be temp or TempC does it matter?
-void ProcessedData::convertTempToCelcius(){
-   
-    double TempF = SensorData.getTemp();
-   
-    double temp = (5.0/9.0) * (TempF - 32.0);
+void DataProcessor::convertTempToCelcius(){
+    
+    double TempF;
+    double TempC;
+    
+    for(auto& dataElement: ProcessedData) {
+        TempF = dataElement.getTemp();
+        TempC = (5.0/9.0) * (TempF - 32.0);
+        dataElement.setTemp(TempC);
+    }
 
-    SensorData.setTemp(temp);
 }
 
 
