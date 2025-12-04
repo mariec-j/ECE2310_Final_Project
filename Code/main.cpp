ef3dba3bbc8d31b407ebe4bfdaf368fdb5d6b19b
mariec-j <mcjardiniano@cpp.edu>
2025-12-03 18:41:48 -0800

    Create main.cpp

---
+++ Code/main.cpp
@@ -0,0 +1,18 @@
+#include "CSVReader.h"
+#include "DataProcessor.h"
+#include "FileWriter.h"
+#include "SensorData.h"
+// #include "SensorHub.h"
+#include <iostream>
+#include <map>
+#include <vector>
+#include <list>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+
+    return 0;
+}
