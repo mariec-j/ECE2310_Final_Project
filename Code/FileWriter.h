30b25574a9e116b5c605d855060ac66a35e814d2
IalwaysLai <llai@cpp.edu>
2025-12-03 16:27:31 -0800

    Create FileWriter.h

---
+++ Code/FileWriter.h
@@ -0,0 +1,22 @@
+#ifndef FILEWRITER_H
+#define FILEWRITER_H
+#include <string>
+#include <vector>
+#include "SensorData.h"
+using namespace std;
+class FileWriter{
+    private: 
+        string outputDirectory;
+
+    public:
+        FileWriter (const string& outputDirectory);
+
+        string getOutputDirectory() const;
+
+        void setOUtputDirectory(const string& directory);
+
+        void writeData(const vector<SensorData>& data, const string& fileName) const;
+        void writeByAttribute(const vector<SensorData>& data, const string& attribute) const;
+};
+
+#endif
